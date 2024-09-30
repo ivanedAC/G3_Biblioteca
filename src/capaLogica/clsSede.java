8e791ca7c8fa893ee7e9c2af9ae0cbb1f0db4608
Alesissss <gfake040305@gmail.com>
2024-09-29 23:26:54 -0500

    Merge origin/master
    # Conflicts:
    #	src/capaCliente/FrmMenuPrincipal.java

---
+++ src/capaLogica/clsSede.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package capaLogica;
+
+import capaDatos.clsJDBC;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author ACER
+ */
+public class clsSede {
+
+    clsJDBC objConectar = new clsJDBC();
+    String strSQL;
+    ResultSet rs = null;
+
+    public ResultSet listarSede() throws Exception {
+        strSQL = "SELECT * FROM SEDE";
+        try {
+            rs = objConectar.consultar(strSQL);
+            return rs;
+        } catch (Exception e) {
+            throw new Exception("Error al listar sedes: " + e.getMessage());
+        }
+    }
+    
+    public Integer obtenerSede(String nom) throws Exception {
+        strSQL = "SELECT * FROM SEDE WHERE NOMBRE ilike " + nom;
+        try {
+            rs = objConectar.consultar(strSQL);
+            if (rs.next()) {
+                return rs.getInt("codigo");
+            }
+        } catch (Exception e) {
+            throw new Exception("Error al obtener sede: " + e.getMessage());
+        }
+        return 0;
+    }
+
+}
