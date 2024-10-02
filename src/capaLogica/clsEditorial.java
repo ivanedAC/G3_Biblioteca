c0b826f4ad06cb0f75e8eaf95dacf1d9c0840753
Anderson Baca <167532651+BanderJ@users.noreply.github.com>
2024-10-02 12:59:04 -0500

    Update clsEditorial.java

--- src/capaLogica/clsEditorial.java
+++ src/capaLogica/clsEditorial.java
@@ -25,4 +25,62 @@ public class clsEditorial {
         }
     }
     
+    public void insertarEditorial(int cod, String nom, boolean vig) throws Exception{
+        strSQL = "select * from pa_insert_editorial ("+cod+", '"+nom+"', "+vig+")";
+        try {
+            objConectar.ejecutar(strSQL);
+        } catch (Exception e) {
+            throw new Exception("Erro al regitrar --->" +e.getMessage());
+        }
+    }
+    
+    public void modficarEditorial(int cod, String nom, boolean vig) throws Exception{
+        strSQL = "select * from pa_update_editorial ("+cod+", '"+nom+"', "+vig+")";
+        try {
+            objConectar.ejecutar(strSQL);
+        } catch (Exception e) {
+            throw new Exception("Error a modificar --->" + e.getMessage());
+        }
+    }
+    
+    public void eliminarEditorial(int cod) throws Exception{
+        strSQL= "select * from pa_delete_editorial("+cod+")";
+        try {
+            objConectar.ejecutar(strSQL);
+        } catch (Exception e) {
+            throw new Exception("Error al eliminar ---> " + e.getMessage());
+        }
+    }
+    
+    public void darBajaEditorial(int cod) throws Exception{
+        strSQL= "select * from pa_disabled_editorial("+cod+")";
+        try {
+            objConectar.ejecutar(strSQL);
+        } catch (Exception e) {
+            throw new Exception("Error al dar de baja --->" +e.getMessage());
+        }
+    }
+    
+    public ResultSet buscarEditorial(int cod) throws Exception{
+        strSQL = "select * from editorial where codigo="+cod;
+        try {
+            rs=objConectar.consultar(strSQL);
+            return rs;
+        } catch (Exception e) {
+            throw new Exception("Error al buscar --->" +e.getMessage());
+        }
+    }
+    
+    public Integer generarCodigoEditorial() throws Exception{
+        strSQL = "select COALESCE(MAX(codigo), 0) + 1 as codigo from editorial";
+        try {
+            rs = objConectar.consultar(strSQL);
+            while (rs.next()) {                
+                return rs.getInt("codigo");
+            }
+        } catch (Exception e) {
+            throw new Exception("Error al generar codigo --->" +e.getMessage());
+        }
+        return 0;
+    }
 }
