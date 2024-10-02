74d6a2a66364c893d5b7c20a33865a1a873f82e5
ivanedAC <166539833+ivanedAC@users.noreply.github.com>
2024-10-01 20:12:20 -0500

    Add files via upload

---
+++ src/capaLogica/clsEditorial.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package capaLogica;
+
+import capaDatos.clsJDBC;
+import java.sql.ResultSet;
+/**
+ *
+ * @author IVAN
+ */
+public class clsEditorial {
+    clsJDBC objConectar = new clsJDBC();
+    String strSQL;
+    ResultSet rs = null;
+    
+    public ResultSet listarEditorial() throws Exception {
+        strSQL = "SELECT * FROM EDITORIAL";
+        try {
+            rs = objConectar.consultar(strSQL);
+            return rs;
+        } catch (Exception e) {
+            throw new Exception("Error al listar las editoriales: "+e.getMessage());
+        }
+    }
+    
+}
