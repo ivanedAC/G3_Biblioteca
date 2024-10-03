b2db8297a28ec9b176446c54a3baf9d9fbabce91
Anderson Baca <167532651+BanderJ@users.noreply.github.com>
2024-10-03 04:13:37 -0500

    Add files via upload
    terminado

--- src/capaLogica/clsEjemplar.java
+++ src/capaLogica/clsEjemplar.java
@@ -17,28 +17,8 @@ public class clsEjemplar {
     clsJDBC objConetar = new clsJDBC();
     ResultSet rs = null;
 
-    public ResultSet listarEjemplaresSede() throws Exception {
-        strSQL = "select * from  listarEjemplaresAsignados";
-        try {
-            rs = objConetar.consultar(strSQL);
-            return rs;
-        } catch (Exception e) {
-            throw new Exception("Error al listar Ejemplares en sede ---> " + e.getMessage());
-        }
-    }
-
-    public ResultSet listarEjemplaresSinA() throws Exception {
-        strSQL = "select * from  listarEjemplaresSinA";
-        try {
-            rs = objConetar.consultar(strSQL);
-            return rs;
-        } catch (Exception e) {
-            throw new Exception("Error al listar Ejemplares sin asignar ---> " + e.getMessage());
-        }
-    }
-    
     public ResultSet listarEjemplarSedeIndiv() throws Exception{
-        strSQL = "select * from listarEjemplaresIndiv";
+        strSQL = "select * from listadoEjemplares";
         try {
             rs = objConetar.consultar(strSQL);
             return rs;
@@ -47,60 +27,32 @@ public class clsEjemplar {
         }
     }
 
-    public ResultSet obtenerCodigoEjemplar(String libro, String sede) throws Exception {
-        strSQL = "select * from obtenerCodigosLibros('" + libro + "','" + sede + "')";
+    public ResultSet buscarPorCodigo(int cod) throws Exception {
+        strSQL = "select * from listadoEjemplares where codigo="+cod;
         try {
             rs = objConetar.consultar(strSQL);
             return rs;
         } catch (Exception e) {
-            throw new Exception("Error al obterner codigo(os) ---> " + e.getMessage());
+            throw new Exception("Error al obterner codigo ---> " + e.getMessage());
         }
     }
     
-    public ResultSet obtenerCodigoEjemplar(String libro) throws Exception{
-        strSQL = "select * from obtenerCodigosLibros('" + libro+"')";
+    public void darBajaEditorial(int cod) throws Exception{
+        strSQL = "select pa_disabled_ejemplar("+cod+")";
         try {
-            rs = objConetar.consultar(strSQL);
-            return rs;
+            objConetar.consultar(strSQL);
         } catch (Exception e) {
-            throw new Exception("Error al obtener codigo(os) ---> " + e.getMessage());
-        }
-    }
-
-    public ResultSet buscarPorCodigo(int[] cod) throws Exception {
-        StringBuilder codigosConcatenados = new StringBuilder();
-        codigosConcatenados.append("ARRAY[");
-
-        for (int i = 0; i < cod.length; i++) {
-            codigosConcatenados.append(cod[i]);
-            if (i < cod.length - 1) {
-                codigosConcatenados.append(", ");
-            }
-        }
-
-        codigosConcatenados.append("]");
-
-        String strSQL = "select * from pa_buscar_libros_por_codigos(" + codigosConcatenados.toString() + ")";
-
-        try {
-            rs = objConetar.consultar(strSQL);
-            return rs;
-        } catch (Exception e) {
-            throw new Exception("Error al buscar por códigos --> " + e.getMessage());
+            throw new Exception("Error al dar de baja ---> " +e.getMessage());
         }
     }
     
-    public Integer generarCodigo() throws Exception{
-        strSQL = "select COALESCE(MAX(codigo), 0) + 1 as codigo from ejemplar";
+    public void modificarEjemplar(int cod, int sede, String est) throws Exception{
+        strSQL = "select pa_update_ejemplar("+cod+","+sede+",'"+est+"')";
         try {
-            rs = objConetar.consultar(strSQL);
-            while (rs.next()) {                
-                return rs.getInt("codigo");
-            }
+            objConetar.consultar(strSQL);
         } catch (Exception e) {
-            throw new Exception("Error al generar codigo --->" +e.getMessage());
+            throw new Exception("Error al modificar ---> "+e.getMessage());
         }
-        return 0;
     }
 
 }
