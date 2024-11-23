5af99515ff9bd38d3a9dd4f8fd40af063095bdc7
Anderson Baca <167532651+BanderJ@users.noreply.github.com>
2024-11-23 18:42:08 -0500

    Update clsEjemplar.java
    metodos de búsqueda para los filtros

--- src/capaLogica/clsEjemplar.java
+++ src/capaLogica/clsEjemplar.java
@@ -65,4 +65,103 @@ public class clsEjemplar {
         }
     }
 
+    public ResultSet buscarEjemplares(String isbn, String libro, String autor, int codEditorial, int codTipoL, int codSede) throws Exception{
+        strSQL = "SELECT "
+                + "li.isbn, "
+                + "li.nombre AS libro, "
+                + "au.nombre AS autor, "
+                + "li.edicion, "
+                + "ed.nombre AS editorial, "
+                + "SUM(CASE WHEN ej.estado = 'D' THEN 1 ELSE 0 END) AS Disponible, "
+                + "SUM(CASE WHEN ej.estado = 'P' THEN 1 ELSE 0 END) AS Prestados "
+                + "FROM (SELECT * FROM libro WHERE isbn LIKE '%"+isbn+"%' AND UPPER(nombre) LIKE UPPER('%"+libro+"%') AND cod_tipo_libro="+codTipoL+") li "
+                + "INNER JOIN ejemplar ej ON ej.isbn = li.isbn "
+                + "INNER JOIN autor_libro auli ON auli.isbn = li.isbn "
+                + "INNER JOIN autor au ON au.codigo = auli.autorcodigo "
+                + "INNER JOIN sede se on se.codigo = ej.cod_sede "
+                + "INNER JOIN editorial ed on ed.codigo = li.cod_editorial "
+                + "WHERE ej.estado IN ('D', 'P') AND se.codigo="+codSede+"  AND UPPER(au.nombre) LIKE UPPER('%"+autor+"%') AND ed.codigo="+codEditorial+" "
+                + "GROUP BY li.isbn, li.nombre, au.nombre, li.edicion, ed.nombre;";
+        try {
+            rs = objConetar.consultar(strSQL);
+            return rs;
+        } catch (Exception e) {
+            throw new Exception("Error al buscar ejemplares -->"+e.getMessage());
+        }
+    }
+    
+    public ResultSet buscarEjemplaresTexto(String isbn, String libro, String autor, int codSede) throws Exception{
+        strSQL = "SELECT "
+                + "li.isbn, "
+                + "li.nombre AS libro, "
+                + "au.nombre AS autor, "
+                + "li.edicion, "
+                + "ed.nombre AS editorial, "
+                + "SUM(CASE WHEN ej.estado = 'D' THEN 1 ELSE 0 END) AS Disponible, "
+                + "SUM(CASE WHEN ej.estado = 'P' THEN 1 ELSE 0 END) AS Prestados "
+                + "FROM (SELECT * FROM libro WHERE isbn LIKE '%"+isbn+"%' AND UPPER(nombre) LIKE UPPER('%"+libro+"%')) li "
+                + "INNER JOIN ejemplar ej ON ej.isbn = li.isbn "
+                + "INNER JOIN autor_libro auli ON auli.isbn = li.isbn "
+                + "INNER JOIN autor au ON au.codigo = auli.autorcodigo "
+                + "INNER JOIN sede se on se.codigo = ej.cod_sede "
+                + "INNER JOIN editorial ed on ed.codigo = li.cod_editorial "
+                + "WHERE ej.estado IN ('D', 'P') AND se.codigo="+codSede+"  AND UPPER(au.nombre) LIKE UPPER('%"+autor+"%') "
+                + "GROUP BY li.isbn, li.nombre, au.nombre, li.edicion, ed.nombre;";
+        try {
+            rs = objConetar.consultar(strSQL);
+            return rs;
+        } catch (Exception e) {
+            throw new Exception("Error al buscar ejemplares por datos ingresados -->"+e.getMessage());
+        }
+    }
+    
+    public ResultSet buscarEjemplaresTipoLibro(String isbn, String libro, String autor, int codTipoL, int codSede) throws Exception{
+        strSQL = "SELECT "
+                + "li.isbn, "
+                + "li.nombre AS libro, "
+                + "au.nombre AS autor, "
+                + "li.edicion, "
+                + "ed.nombre AS editorial, "
+                + "SUM(CASE WHEN ej.estado = 'D' THEN 1 ELSE 0 END) AS Disponible, "
+                + "SUM(CASE WHEN ej.estado = 'P' THEN 1 ELSE 0 END) AS Prestados "
+                + "FROM (SELECT * FROM libro WHERE isbn LIKE '%"+isbn+"%' AND UPPER(nombre) LIKE UPPER('%"+libro+"%') AND cod_tipo_libro="+codTipoL+") li "
+                + "INNER JOIN ejemplar ej ON ej.isbn = li.isbn "
+                + "INNER JOIN autor_libro auli ON auli.isbn = li.isbn "
+                + "INNER JOIN autor au ON au.codigo = auli.autorcodigo "
+                + "INNER JOIN sede se on se.codigo = ej.cod_sede "
+                + "INNER JOIN editorial ed on ed.codigo = li.cod_editorial "
+                + "WHERE ej.estado IN ('D', 'P') AND se.codigo="+codSede+"  AND UPPER(au.nombre) LIKE UPPER('%"+autor+"%') "
+                + "GROUP BY li.isbn, li.nombre, au.nombre, li.edicion, ed.nombre;";
+        try {
+            rs = objConetar.consultar(strSQL);
+            return rs;
+        } catch (Exception e) {
+            throw new Exception("Error al buscar ejemplares -->"+e.getMessage());
+        }
+    }
+    
+    public ResultSet buscarEjemplaresEditorial(String isbn, String libro, String autor, int codEditorial, int codSede) throws Exception{
+        strSQL = "SELECT "
+                + "li.isbn, "
+                + "li.nombre AS libro, "
+                + "au.nombre AS autor, "
+                + "li.edicion, "
+                + "ed.nombre AS editorial, "
+                + "SUM(CASE WHEN ej.estado = 'D' THEN 1 ELSE 0 END) AS Disponible, "
+                + "SUM(CASE WHEN ej.estado = 'P' THEN 1 ELSE 0 END) AS Prestados "
+                + "FROM (SELECT * FROM libro WHERE isbn LIKE '%"+isbn+"%' AND UPPER(nombre) LIKE UPPER('%"+libro+"%')) li "
+                + "INNER JOIN ejemplar ej ON ej.isbn = li.isbn "
+                + "INNER JOIN autor_libro auli ON auli.isbn = li.isbn "
+                + "INNER JOIN autor au ON au.codigo = auli.autorcodigo "
+                + "INNER JOIN sede se on se.codigo = ej.cod_sede "
+                + "INNER JOIN editorial ed on ed.codigo = li.cod_editorial "
+                + "WHERE ej.estado IN ('D', 'P') AND se.codigo="+codSede+"  AND UPPER(au.nombre) LIKE UPPER('%"+autor+"%') AND ed.codigo="+codEditorial+" "
+                + "GROUP BY li.isbn, li.nombre, au.nombre, li.edicion, ed.nombre;";
+        try {
+            rs = objConetar.consultar(strSQL);
+            return rs;
+        } catch (Exception e) {
+            throw new Exception("Error al buscar ejemplares -->"+e.getMessage());
+        }
+    }
 }
